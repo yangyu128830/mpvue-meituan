fb4b6aaf4f1980c6131776711c76e6e632dbcf0d
ysy13 <ysy13044878810@qq.com>
2025-11-26 16:51:58 +0800

    18

---
+++ src/pages/food/components/FoodCategories.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="food-categories">
+    <div class="category-tabs">
+      <div class="tab-item" v-for="(category, index) in categories" :key="index"
+           :class="{active: activeCategory === index}" @click="onCategoryClick(index)">
+        {{ category.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FoodCategories',
+  data() {
+    return {
+      categories: [
+        { name: '全部美食' },
+        { name: '人气推荐' },
+        { name: '限时秒杀' },
+        { name: '学生专区' },
+        { name: '美食排行' }
+      ],
+      activeCategory: 0
+    }
+  },
+  methods: {
+    onCategoryClick(index) {
+      this.activeCategory = index;
+      // Emit event to parent component
+      this.$emit('categoryChange', index);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.food-categories {
+  background-color: #fff;
+  border-bottom: 1px solid #f0f0f0;
+  
+  .category-tabs {
+    display: flex;
+    overflow-x: auto;
+    padding: 0 20rpx;
+    
+    .tab-item {
+      padding: 20rpx 30rpx;
+      font-size: 28rpx;
+      color: #666;
+      white-space: nowrap;
+      transition: color 0.3s ease;
+      
+      &.active {
+        color: #ff4444;
+        font-weight: bold;
+        border-bottom: 3px solid #ff4444;
+      }
+      
+      &:not(:last-child) {
+        margin-right: 20rpx;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/food/components/FoodHeader.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="food-header">
+    <!-- Location Selector -->
+    <div class="location" @click="onLocationClick">
+      <i class="icon mt-location-o"></i>
+      <span>{{ currentLocation }}</span>
+      <i class="icon mt-arrow-right-o"></i>
+    </div>
+    
+    <!-- Search Bar -->
+    <div class="search-bar" @click="onSearchClick">
+      <i class="icon mt-search-o"></i>
+      <input type="text" placeholder="搜索美食、商家或商品" v-model="searchKeyword" readonly>
+    </div>
+    
+    <!-- Scan Icon -->
+    <div class="scan-icon" @click="onScanClick">
+      <i class="icon mt-scan-o"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FoodHeader',
+  data() {
+    return {
+      currentLocation: '上海市漕河泾开发区',
+      searchKeyword: ''
+    }
+  },
+  methods: {
+    onLocationClick() {
+      // Handle location selection
+    },
+    onSearchClick() {
+      // Handle search
+    },
+    onScanClick() {
+      // Handle scan
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.food-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20rpx;
+  background-color: #fff;
+  border-bottom: 1px solid #f0f0f0;
+  
+  .location {
+    display: flex;
+    align-items: center;
+    color: #333;
+    font-size: 28rpx;
+    
+    i {
+      margin-right: 10rpx;
+      font-size: 32rpx;
+    }
+    
+    i:last-child {
+      margin-left: 10rpx;
+      font-size: 24rpx;
+    }
+  }
+  
+  .search-bar {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    background-color: #f5f5f5;
+    border-radius: 40rpx;
+    padding: 12rpx 20rpx;
+    margin: 0 20rpx;
+    
+    i {
+      color: #999;
+      font-size: 28rpx;
+      margin-right: 10rpx;
+    }
+    
+    input {
+      flex: 1;
+      background-color: transparent;
+      border: none;
+      outline: none;
+      font-size: 28rpx;
+      color: #333;
+    }
+  }
+  
+  .scan-icon {
+    width: 60rpx;
+    height: 60rpx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #ff4444;
+    border-radius: 50%;
+    
+    i {
+      color: #fff;
+      font-size: 32rpx;
+    }
+  }
+}
+</style>
